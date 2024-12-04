28ee479c4b31fbc811ae98596418a560fe13b1c3
Howida <howiidaelsafoury@gmail.com>
2024-12-04 23:25:48 +0200

    finLal

--- lib/pages/tab_bar_list_view.dart
+++ lib/pages/tab_bar_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/source_model.dart';
+import 'package:news_app/widgets/article_list_view.dart';
 import 'package:news_app/widgets/tab_item.dart';
 
 class TabBarListView extends StatefulWidget {
@@ -12,29 +13,36 @@ class TabBarListView extends StatefulWidget {
 }
 
 class _TabBarListViewState extends State<TabBarListView> {
-  int selectedIndex=0;
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-
-      initialIndex: selectedIndex,
-      length: widget.sourceModel.sources?.length ?? 0,
-      child: TabBar(
-        labelPadding:const EdgeInsets.symmetric(horizontal: 4),
-        indicator: const BoxDecoration(border: Border(bottom: BorderSide.none)),
-        onTap: (index){
-          setState(() {
-            selectedIndex=index;
-          });
-        },
-        isScrollable: true,
-
-        tabs: widget.sourceModel.sources?.map((element){
-        return TabItem(element,selectedIndex == widget.sourceModel.sources!.indexOf(element) );
-      }).toList()?? [],
-      ),
-
+    return Column(
+      children: [
+        DefaultTabController(
+          initialIndex: selectedIndex,
+          length: widget.sourceModel.sources?.length ?? 0,
+          child: TabBar(
+            labelPadding: const EdgeInsets.symmetric(horizontal: 4),
+            indicator:
+                const BoxDecoration(border: Border(bottom: BorderSide.none)),
+            onTap: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            isScrollable: true,
+            tabs: widget.sourceModel.sources?.map((element) {
+                  return TabItem(
+                      element,
+                      selectedIndex ==
+                          widget.sourceModel.sources!.indexOf(element));
+                }).toList() ??
+                [],
+          ),
+        ),
+       ArticleListView(widget.sourceModel.sources?[selectedIndex].id ?? "")
+      ],
     );
   }
 }
---
+++ lib/widgets/article_list_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/core/network_layer/api_manager.dart';
+import 'package:news_app/pages/article_view.dart';
+
+class ArticleListView extends StatelessWidget {
+  String sourceId;
+  ArticleListView(this.sourceId);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(future: ApiManager.fetchArticles(sourceId),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Column(
+              children: [
+                Text("${snapshot.error}"),
+                IconButton(
+                  onPressed: () {
+                  },
+                  icon: const Icon(Icons.refresh_outlined),
+                ),
+              ],
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else{
+            var articleList=snapshot.data?.articles ?? [];
+            return Expanded(
+              child: ListView.builder(itemBuilder: (context, index) {
+                return ArticleView(articleList[index]);
+              },
+              itemCount: 10,),
+            );
+          }
+        },);
+
+  }
+}
